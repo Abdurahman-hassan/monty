203612d67bae1388f09050196594ee3798b5dbf9
mojtaba <mojtaba.mohamed187@gmail.com>
2023-11-15 21:41:45 +0200

    Add main.c, get_function.c, monty.h and stack_functions files

---
+++ main.c
@@ -0,0 +1,84 @@
+#include "monty.h"
+
+stack_t *head = NULL;
+
+/**
+ * main - Entry point for the Monty interpreter program.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Description:
+ *   The main function serves as the entry point for the Monty interpreter
+ *   program. It checks for the correct number of command-line arguments,
+ *   opens and processes the specified file, and then frees the memory
+ *   allocated for the stack. If the expected number of arguments is not
+ *   provided, an error message is printed to stderr, and the program exits
+ *   with EXIT_FAILURE.
+ *
+ * Return: 0 on successful execution.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		perror("USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+
+	open_file(argv[1]);
+
+	free_stack(&head);
+	return (0);
+}
+
+/**
+ * open_file - Opens and reads a file containing stack operations.
+ *
+ * @filename: The name of the file to be opened and processed.
+ *
+ * Description:
+ *   This function opens the specified file, reads each line, and
+ *   processes the stack operations accordingly. It supports the "push"
+ *   operation with integer values. If an error occurs during file
+ *   opening or processing, an error message is printed to stderr,
+ *   and the program exits with EXIT_FAILURE.
+ *
+ * Return: Nothing.
+ */
+void open_file(char *filename)
+{
+	FILE *file_pointer = fopen(filename, "r");
+	char *line_buffer = NULL, *opcode = NULL, *value = NULL;
+	int line_number = 1;
+	size_t line_size = 0;
+	int *stack_value = get_stack_val();
+
+	if (file_pointer == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	for (; getline(&line_buffer, &line_size, file_pointer) != -1; line_number++)
+	{
+		if (*line_buffer == '\n')
+			continue;
+		line_buffer[strcspn(line_buffer, "\n")] = '\0';
+		opcode = strtok(line_buffer, " ");
+		value = strtok(NULL, " ");
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (atoi(value) == 0)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			*stack_value = atoi(value);
+		}
+		get_func(opcode, &head, line_number);
+	}
+
+	free(line_buffer);
+	fclose(file_pointer);
+}
---
+++ stack_functions.c
@@ -0,0 +1,128 @@
+#include "monty.h"
+
+/**
+ * push - Adds a new node with a given value to the top of the stack.
+ *
+ * @stack_head: A double pointer to the head of the stack.
+ * @line_number: The line number in the input file.
+ *
+ * Description:
+ *   This function creates a new node with the value obtained from the
+ *   stack, and adds it to the top of the stack. If memory allocation fails,
+ *   an error message is printed, and the program exits with EXIT_FAILURE.
+ *
+ * Return: Nothing.
+ */
+void push(stack_t **stack_head, unsigned int line_number)
+{
+	int *value = get_stack_val();
+	stack_t *current_node = NULL;
+	stack_t *new_node = malloc(sizeof(stack_t));
+	(void) line_number;
+
+	if (!new_node)
+	{
+		perror("Error: malloc failed");
+		free_stack(stack_head);
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = *value;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (*stack_head == NULL)
+	{
+		*stack_head = new_node;
+	}
+	else
+	{
+		current_node = *stack_head;
+		while (current_node->next)
+			current_node = current_node->next;
+
+		current_node->next = new_node;
+		new_node->prev = current_node;
+	}
+
+}
+
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ *
+ * @stack_head: A double pointer to the head of the stack.
+ * @line_number: The line number in the input file.
+ *
+ * Description:
+ *   This function prints all the values on the stack, starting from
+ *   the top. If the stack is empty, it does nothing. It traverses the
+ *   stack from the top to the bottom and prints each node's value.
+ *
+ * Return: Nothing.
+ */
+void pall(stack_t **stack_head, unsigned int line_number)
+{
+	stack_t *current_node = *stack_head;
+	(void) line_number;
+
+	if (current_node && !current_node->next)
+	{
+		printf("%d\n", current_node->n);
+		return;
+	}
+
+	while (current_node->next)
+		current_node = current_node->next;
+
+	while (current_node)
+	{
+		printf("%d\n", current_node->n);
+		current_node = current_node->prev;
+	}
+}
+
+/**
+ * get_stack_val - Retrieves the address of a static variable for stack values.
+ *
+ * Description:
+ *   This function returns the address of a static variable used for storing
+ *   values related to stack operations. The static variable retains its
+ *   value between function calls, allowing for shared access to a common
+ *   stack-related value.
+ *
+ * Return: A pointer to the static integer variable storing stack values.
+ */
+int *get_stack_val()
+{
+	static int value = 1;
+
+	return (&value);
+}
+
+/**
+ * free_stack - Frees all the nodes in a stack and sets the head to NULL.
+ *
+ * @stack_head: A double pointer to the head of the stack.
+ *
+ * Description:
+ *   This function frees all the nodes in the stack, starting from the
+ *   head, and sets the head pointer to NULL. If the stack or head
+ *   pointer is already NULL, it does nothing.
+ *
+ * Return: Nothing.
+ */
+void free_stack(stack_t **stack_head)
+{
+	stack_t *curr;
+
+	if (!*stack_head || !stack_head)
+		return;
+
+	while (*stack_head)
+	{
+		curr = *stack_head;
+		*stack_head = (*stack_head)->next;
+		free(curr);
+	}
+
+	*stack_head = NULL;
+}
